c944f8d7f3300c903d8e6419606d786caba26373
Tomáš Hujčák <109172751+hujco@users.noreply.github.com>
2025-03-25 11:36:25 +0100

    Update callback.js

--- callback.js
+++ callback.js
@@ -5,9 +5,20 @@ module.exports = function(oauth2, provider) {
 
   router.get('/', async (req, res) => {
     const code = req.query.code;
+    if (!code) {
+      console.error('No code provided.');
+      return res.status(400).json('No code provided');
+    }
+
+    const redirect_uri = process.env.REDIRECT_URI;
+    if (!redirect_uri) {
+      console.error('Environment REDIRECT_URI is not set.');
+      return res.status(500).json('Server misconfiguration');
+    }
+
     const options = {
       code,
-      redirect_uri: process.env.REDIRECT_URI,
+      redirect_uri,
     };
 
     try {
@@ -15,7 +26,7 @@ module.exports = function(oauth2, provider) {
       const token = result.token.access_token;
       res.redirect(`/success#access_token=${token}&provider=${provider}`);
     } catch (error) {
-      console.error('Access Token Error', error.message);
+      console.error('Access Token Error:', error.message);
       res.status(500).json('Authentication failed');
     }
   });
