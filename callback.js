bd4691028319baca2dacc7fdefd59f01319f0286
Tomáš Hujčák <109172751+hujco@users.noreply.github.com>
2025-03-25 10:42:06 +0100

    Update callback.js

--- callback.js
+++ callback.js
@@ -1,36 +1,24 @@
-const generateScript = require('./login_script.js')
+const express = require('express');
 
-module.exports = (oauth2, oauthProvider) => {
-  function callbackMiddleWare (req, res, next) {
-    const code = req.query.code
-    var options = {
-      code: code
+module.exports = function(oauth2, provider) {
+  const router = express.Router();
+
+  router.get('/', async (req, res) => {
+    const code = req.query.code;
+    const options = {
+      code,
+      redirect_uri: process.env.REDIRECT_URI,
+    };
+
+    try {
+      const result = await oauth2.getToken(options);
+      const token = result.token.access_token;
+      res.redirect(`/success#access_token=${token}&provider=${provider}`);
+    } catch (error) {
+      console.error('Access Token Error', error.message);
+      res.status(500).json('Authentication failed');
     }
+  });
 
-    if (oauthProvider === 'gitlab') {
-      options.client_id = process.env.OAUTH_CLIENT_ID
-      options.client_secret = process.env.OAUTH_CLIENT_SECRET
-      options.grant_type = 'authorization_code'
-      options.redirect_uri = process.env.REDIRECT_URL
-    }
-
-    oauth2.getToken(options)
-      .then(result => {
-        const token = oauth2.createToken(result)
-        content = {
-          token: token.token.token.access_token,
-          provider: oauthProvider
-        }
-        return { message: 'success', content }
-      })
-      .catch(error => {
-        console.error('Access Token Error', error.message)
-        return { message: 'error', content: JSON.stringify(error) }
-      })
-      .then(result => {
-        const script = generateScript(oauthProvider, result.message, result.content)
-        return res.send(script)
-      })
-  }
-  return callbackMiddleWare
-}
+  return router;
+};
