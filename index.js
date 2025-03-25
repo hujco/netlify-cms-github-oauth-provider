f668d8f79d87b1f9a86163e45a6529d4809b45be
Tomáš Hujčák <109172751+hujco@users.noreply.github.com>
2025-03-25 09:57:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
-const simpleOauthModule = require('simple-oauth2')
-const authMiddleWareInit = require('./auth.js')
-const callbackMiddleWareInit = require('./callback')
-const oauthProvider = process.env.OAUTH_PROVIDER || 'github'
-const loginAuthTarget = process.env.AUTH_TARGET || '_self'
+const express = require('express');
+const simpleOauthModule = require('simple-oauth2');
+const authMiddleWareInit = require('./auth.js');
+const callbackMiddleWareInit = require('./callback');
+
+const oauthProvider = process.env.OAUTH_PROVIDER || 'github';
+const loginAuthTarget = process.env.AUTH_TARGET || '_self';
 
 const config = {
   client: {
     id: process.env.OAUTH_CLIENT_ID,
-    secret: process.env.OAUTH_CLIENT_SECRET
+    secret: process.env.OAUTH_CLIENT_SECRET,
   },
   auth: {
-    // Supply GIT_HOSTNAME for enterprise github installs.
     tokenHost: process.env.GIT_HOSTNAME || 'https://github.com',
     tokenPath: process.env.OAUTH_TOKEN_PATH || '/login/oauth/access_token',
-    authorizePath: process.env.OAUTH_AUTHORIZE_PATH || '/login/oauth/authorize'
-  }
-}
+    authorizePath: process.env.OAUTH_AUTHORIZE_PATH || '/login/oauth/authorize',
+  },
+};
 
-const oauth2 = new simpleOauthModule.AuthorizationCode(config)
+const oauth2 = new simpleOauthModule.AuthorizationCode(config);
+const app = express();
 
-function indexMiddleWare (req, res) {
+function indexMiddleware(req, res) {
   res.send(`Hello<br>
     <a href="/auth" target="${loginAuthTarget}">
       Log in with ${oauthProvider.toUpperCase()}
-    </a>`)
+    </a>`);
 }
-
-module.exports = {
-  auth: authMiddleWareInit(oauth2),
-  callback: callbackMiddleWareInit(oauth2, oauthProvider),
-  success: (req, res) => { res.send('') },
-  index: indexMiddleWare
-}
-
-const express = require('express');
-const app = express();
 
 app.use('/', indexMiddleware);
+
+app.use('/auth', authMiddleWareInit(oauth2));
+app.use('/callback', callbackMiddleWareInit(oauth2, oauthProvider));
+app.use('/success', (req, res) => {
+  res.send('');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
